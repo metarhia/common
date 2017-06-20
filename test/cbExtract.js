4160acd2e77a3673e533ae68c8530595d369dbd0
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-20 22:04:22 +0300

    fixup! Add tests for cbExtract

--- test/cbExtract.js
+++ test/cbExtract.js
@@ -18,7 +18,6 @@ tap.test('cbExctract', (test) => {
 
 tap.test('cbExctract without callback', (test) => {
   const expectedArgs = [1, 2, 3];
-  const expectedCallbackArgs = [4, 5, 6];
   const args = [1, 2, 3];
   const wrappedCb = common.cbExtract(args);
   test.strictSame(args, expectedArgs);
