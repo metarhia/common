a3da9acc066bb4f02b3e4e9b13ff35d2c6b7f253
Denys Otrishko <shishugi@gmail.com>
2018-08-20 13:41:26 +0300

    fixup! Implement callerFilename, callerFilepath

--- test/utilities.js
+++ test/utilities.js
@@ -1,13 +1,17 @@
 'use strict';
 
-api.metatests.test('Check called filename', (test) => {
+api.metatests.test('Check called filename/filepath', (test) => {
   test.ok(api.common.callerFilepath().endsWith('test/utilities.js'));
   test.strictSame(api.common.callerFilename(), 'utilities.js');
   test.end();
 });
 
-api.metatests.test('Check called filename parent', (test) => {
-  test.ok(api.common.callerFilepath(1).endsWith('test/all.js'));
-  test.strictSame(api.common.callerFilename(1), 'all.js');
+api.metatests.test('Check called filename/filepath parent', (test) => {
+  child(test, 1);
   test.end();
 });
+
+function child(test, depth) {
+  test.ok(api.common.callerFilepath(depth).endsWith('test/utilities.js'));
+  test.strictSame(api.common.callerFilename(depth), 'utilities.js');
+}
