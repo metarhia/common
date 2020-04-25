7a734976fe73fa554975f8166f10cf0eeb969033
arkon <blackdark20@mail.ru>
2020-04-25 12:29:45 +0500

    test pull request

--- test/array.js
+++ test/array.js
@@ -5,7 +5,7 @@ const common = require('..');
 
 metatests.case(
   'Common / array',
-  { common },
+  { commonшншнгшгш },
   {
     'common.splitAt': [
       [0, [1, 2, 3, 4, 5], [[], [1, 2, 3, 4, 5]]],
